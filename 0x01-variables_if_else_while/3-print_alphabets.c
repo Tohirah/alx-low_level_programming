01f956bf9eab53e4a08217431a705c7c54666cbb
Tohirah <tohirahbankole@gmail.com>
2023-07-14 21:12:14 -0700

    doing hard things

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,9 +2,9 @@
 
 /**
  *  main - Prints the alphabet.
- * 
+ *
  *  Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
