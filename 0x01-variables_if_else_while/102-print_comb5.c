1b8a13768c4e839be7aa58bfcb5a47591b64b319
Tohirah <tohirahbankole@gmail.com>
2023-07-14 21:36:45 -0700

    Low level lang fr

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,7 @@
 */
  
 int main(void)
-
+	 
 {
 	       int p, q;
 	        
@@ -16,17 +16,17 @@ int main(void)
 		      {
 		      	for (q = p + 1; q <= 99; q++)
 			{
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
-				putchar(' ');
-				putchar((q / 10) + '0');
-				putchar((q % 10) + '0');
-				if (p == 98 && q == 99)
-				continue;
-				putchar(',');
-				putchar(' ');
-			}
+			   	putchar((p / 10) + '0');
+			        putchar((p % 10) + '0');
+			    	putchar(' ');
+			     	putchar((q / 10) + '0');
+			     	putchar((q % 10) + '0');
+			     	if (p == 98 && q == 99)
+			     	continue;
+			     	putchar(',');
+			    	putchar(' ');
+			    }	
 		      }
 		putchar('\n');
-		return (0);
+		 return (0);
 }
