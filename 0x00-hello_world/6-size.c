aaba3e5a45b30cfc73dd296ed3d3b0bd18a341e5
Tohirah <tohirahbankole@gmail.com>
2023-07-16 04:49:35 -0700

    finally prints

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *   *        the computer it is compiled and run on..
  *    *
  *     * Return: Always 0.
- *      */
+ */
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -13,6 +13,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	
+
 	return (0);
 }
