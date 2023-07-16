81bdd2e12c183dac03a29bfd95ce39519fca187f
Tohirah <tohirahbankole@gmail.com>
2023-07-16 15:10:02 -0700

    I tried printing

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  * main - Prints all possible combinations of single-digit numbers.
+ *   *
+ *    * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
