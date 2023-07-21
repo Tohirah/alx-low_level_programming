ac367b1e60656b5b41b563b297f83484fe4d3685
Tohirah <tohirahbankole@gmail.com>
2023-07-21 15:41:27 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 1 to 9
+ * Return: return 0
+ */
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
