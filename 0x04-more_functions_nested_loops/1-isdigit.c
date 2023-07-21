8a0b6c5588a68c8f254759c43881517261947117
Tohirah <tohirahbankole@gmail.com>
2023-07-21 15:29:11 -0700

    digit checker and mul

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - fucntion that checks for a digit
+ * @c: number to be checked
+ * Return: return 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: num1
+ * @b: num2
+ * Return: return multiplied number
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
