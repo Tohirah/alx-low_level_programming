2b771b2ec2b0d39d378db6f0a85204249f172c23
Tohirah <tohirahbankole@gmail.com>
2023-07-20 17:15:21 -0700

    print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer with _putchar
+ * @n - integer to be printed
+ */
+
+void print_number(int n)
+{
+	char c = n;
+
+	_putchar(c);
+
+	return (0);
+}
