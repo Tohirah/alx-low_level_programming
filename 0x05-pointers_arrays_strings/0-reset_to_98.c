56c4aea739a8c46a9f958f448d614a44042ed75a
Tohirah <tohirahbankole@gmail.com>
2023-07-24 14:06:41 -0700

    reset with pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it is pointing to
+ * @n: the pointer itself
+ *
+ * Return: return 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers points to the address of the variable and they can ne used to change the value of the variable
