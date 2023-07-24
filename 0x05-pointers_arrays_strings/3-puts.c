4d535d44b79eb038bb3f719c4b5be482bb29fcd6
Tohirah <tohirahbankole@gmail.com>
2023-07-24 14:31:05 -0700

    count length and print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ *
+ * Return: return 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
