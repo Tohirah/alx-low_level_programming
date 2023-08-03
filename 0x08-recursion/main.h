06df7b406db225a6e4bfafc01b765c4ebc863bf0
Tohirah <tohirahbankole@gmail.com>
2023-08-03 15:02:28 -0700

    palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _strlen_recursion(char *len);
+int palindrome(char *txt, int ini, int end);
+int _check_asterisk(char *s3, int end_1);
+int _equal_no_aster(char *s4, char *s5, int cont_2);
+int identical_asterisk(char *s6, char *s7, int cont_3, int cont_4);
