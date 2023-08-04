0664cf24983e6f3d4a7c6acc0eb3559817be64b2
Tohirah <tohirahbankole@gmail.com>
2023-08-04 10:39:50 -0700

    print argument cpunt

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A program that prints its name
+ * @argc: argument count
+ * @argv: argument vector or array
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints argument count
+ *@ argc: argument count
+ * @argv: argument array
+ */
+
+int main(int argc, char *argv[])
+{
+	int count, num = 0;
+
+	for (count = 0; count < argc; count++)
+	{
+		if count == 0;
+		continue;
+
+		num += 1;
+	}
+
+		printf("%d\n", count - 1);
+
+	return (0);
+}
+
