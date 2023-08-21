c632c1ff5b717e69f6f42f0cc09db94657dc30ce
Tohirah <tohirahbankole@gmail.com>
2023-08-21 11:47:05 -0700

    dog data type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 /**
-*struct dog - new data type dog
+* struct dog - god data type
 * @name: name of dog
 * @age: age of dog
 * @owner: owner's name
@@ -17,20 +17,3 @@ struct dog
 	float age;
 	char *owner;
 };
-
-
-/**
-*dog_t - Typedef for struct dog
-*/
-
-typedef struct dog dog_t;
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
-
-#endif
